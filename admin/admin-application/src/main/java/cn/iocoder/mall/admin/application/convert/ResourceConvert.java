3b97b58df70a228af701ba711e9d9a05a7dfd349
YunaiV <>
2019-03-01 00:34:30 +0800

    资源列表 API 提供

--- admin/admin-application/src/main/java/cn/iocoder/mall/admin/application/convert/ResourceConvert.java
+++ admin/admin-application/src/main/java/cn/iocoder/mall/admin/application/convert/ResourceConvert.java
@@ -1,7 +1,10 @@
 package cn.iocoder.mall.admin.application.convert;
 
+import cn.iocoder.common.framework.vo.CommonResult;
 import cn.iocoder.mall.admin.api.bo.ResourceBO;
 import cn.iocoder.mall.admin.application.vo.AdminMenuTreeNodeVO;
+import cn.iocoder.mall.admin.application.vo.ResourceTreeNodeVO;
+import cn.iocoder.mall.admin.application.vo.ResourceVO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
@@ -13,5 +16,14 @@ public interface ResourceConvert {
 
     @Mappings({})
     AdminMenuTreeNodeVO convert(ResourceBO resourceBO);
+
+    @Mappings({})
+    ResourceTreeNodeVO convert2(ResourceBO resourceBO);
+
+    @Mappings({})
+    ResourceVO convert3(ResourceBO resourceBO);
+
+    @Mappings({})
+    CommonResult<ResourceVO> convert3(CommonResult<ResourceBO> resourceBO);
 
 }

13e9b5526b77978d8cb540943e088d59c9aa41dd
sin <2943460818@qq.com>
2019-03-04 15:30:21 +0800

    格式化代码

--- mobile-web/src/api/page.js
+++ mobile-web/src/api/page.js
@@ -3,15 +3,15 @@ import request from "../config/request";
 
 export function GetPage() {
     return request({
-      url: '/Page/GetPage',
-      method: 'get',
+        url: '/Page/GetPage',
+        method: 'get',
     })
-  }
+}
 
 export function getProduct(id) {
     return request({
-      url: '/Page/Product',
-      method: 'get',
-      params: { id }
+        url: '/Page/Product',
+        method: 'get',
+        params: {id}
     })
-  }
+}

10a1f83c8efe76bb9664b01e66f226c2cf4bc4d9
sin <2943460818@qq.com>
2019-03-05 21:35:29 +0800

    - 添加 mock 数据

--- admin-web/mock/admin.js
+++ admin-web/mock/admin.js
@@ -1,6 +1,7 @@
 import adminMenu from './geographic/admin-menu.json';
 import adminMenuAll from './geographic/admin-menu-all.json';
 import adminUrls from './geographic/admin-urls';
+import resourceTree from './geographic/resource-tree.json';
 
 /* eslint-disable */
 function getAdminMenu(req, res) {
@@ -15,7 +16,12 @@ function getAdminUrls(req, res) {
   return res.json(adminUrls);
 }
 
+function getResourceTree(req, res) {
+  return res.json(resourceTree);
+}
+
 export default {
   'GET /admin-api/admin/resource/admin_menu_tree': getAdminMenuAll,
   'GET /admin-api/admin/resource/admin_url_list': getAdminUrls,
+  'GET /admin-api/admin/resource/tree': getResourceTree,
 };

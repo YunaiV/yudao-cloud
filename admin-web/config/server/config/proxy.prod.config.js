75b133eab8a32452d7fcdc70b040728ad049964e
sin <2943460818@qq.com>
2019-03-10 13:31:49 +0800

    修改 proxy 构建配置，

--- admin-web/config/server/config/proxy.prod.config.js
+++ admin-web/config/server/config/proxy.prod.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  '/admin-api/': {
-    target: 'http://180.167.213.26:18083/',
+  '/admin-api -> http://180.167.213.26:18083/': {
     changeOrigin: true,
     pathRewrite: {},
   },
-  '/server/api/': {
-    target: 'https://preview.pro.ant.design/',
+  '/server/api -> https://preview.pro.ant.design/': {
     changeOrigin: true,
     pathRewrite: { '^/server': '' },
   },

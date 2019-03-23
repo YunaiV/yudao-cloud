67c8dd63116e899669c6836db68cadeef8e648f5
sin <2943460818@qq.com>
2019-03-23 11:38:00 +0800

    添加 ModuleErrorCodeInterval，来定义模块错误码区间

---
+++ common/common-framework/src/main/java/cn/iocoder/common/framework/constant/ModuleErrorCodeInterval.java
@@ -0,0 +1,18 @@
+package cn.iocoder.common.framework.constant;
+
+/**
+ * 模块错误码区间
+ *
+ *  解决：解决各模块错误码定义，避免重复，在此只声明不做实际使用
+ *
+ * @author Sin
+ * @time 2019-03-23 11:28
+ */
+public final class ModuleErrorCodeInterval {
+
+    // order 错误码区间 [8000000 - 8100000]
+
+    // admin 错误码区间 [] TODO 芋艿 admin 错误码区间
+
+    // pay 错误码区间 [] TODO 芋艿 pay 错误码区间
+}

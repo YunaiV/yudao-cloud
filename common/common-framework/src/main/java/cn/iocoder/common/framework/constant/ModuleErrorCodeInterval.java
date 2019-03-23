4fd506895cc76a59cebef12bfecbc1a9d97b670a
sin <2943460818@qq.com>
2019-03-23 11:41:16 +0800

    - 添加错误定义规则

--- common/common-framework/src/main/java/cn/iocoder/common/framework/constant/ModuleErrorCodeInterval.java
+++ common/common-framework/src/main/java/cn/iocoder/common/framework/constant/ModuleErrorCodeInterval.java
@@ -5,12 +5,19 @@ package cn.iocoder.common.framework.constant;
  *
  *  解决：解决各模块错误码定义，避免重复，在此只声明不做实际使用
  *
+ *
+ *  错误码定义说明：
+ *
+ *      [0-000-000-000] 1 开头，给业务系统，2 开头，给框架使用
+ *
+ *      如：用户系统，使用 1-001-000-000 段
+ *
  * @author Sin
  * @time 2019-03-23 11:28
  */
 public final class ModuleErrorCodeInterval {
 
-    // order 错误码区间 [8000000 - 8100000]
+    // order 错误码区间 [1-000-001-000 ~ 1-000-002-000]
 
     // admin 错误码区间 [] TODO 芋艿 admin 错误码区间
 

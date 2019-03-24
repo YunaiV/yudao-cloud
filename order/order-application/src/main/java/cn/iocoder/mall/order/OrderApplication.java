1b200b0f0744e130d0ce116128e4ab54c6714368
sin <2943460818@qq.com>
2019-03-24 11:38:15 +0800

    - 删除多余的 MapperSan

--- order/order-application/src/main/java/cn/iocoder/mall/order/OrderApplication.java
+++ order/order-application/src/main/java/cn/iocoder/mall/order/OrderApplication.java
@@ -1,6 +1,5 @@
 package cn.iocoder.mall.order;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 

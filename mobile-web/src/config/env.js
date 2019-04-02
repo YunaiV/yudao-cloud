3f725e94fdb202e4e8eb3e731720cd63f0a04d90
sin <2943460818@qq.com>
2019-04-02 11:26:56 +0800

    添加 production 环境切换

--- mobile-web/src/config/env.js
+++ mobile-web/src/config/env.js
@@ -11,10 +11,11 @@ let routerMode = 'hash';
 let dataSources='local';//local=本地，其他值代表非本地
 
 
-if (process.env.NODE_ENV == 'development') {
+if (!process.env.NODE_ENV || process.env.NODE_ENV == 'development') {
 	baseUrl='http://127.0.0.1';
 }else if(process.env.NODE_ENV == 'production'){
 	baseUrl = 'http://180.167.213.26:18099';
+    dataSources='production';
 }
 
 // baseUrl = 'http://127.0.0.1';

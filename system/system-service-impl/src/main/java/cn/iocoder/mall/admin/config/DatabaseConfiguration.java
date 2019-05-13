4c923fb20ad8abc55e7050cdc90cd0849ac31c0e
YunaiV <zhijiantianya@gmail.com>
2019-05-13 19:34:43 +0800

    - 后端：整理下 system pom 文件

--- system/system-service-impl/src/main/java/cn/iocoder/mall/admin/config/DatabaseConfiguration.java
+++ system/system-service-impl/src/main/java/cn/iocoder/mall/admin/config/DatabaseConfiguration.java
@@ -9,6 +9,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement(proxyTargetClass = true) // 启动事务管理。为什么使用 proxyTargetClass 参数，参见 https://blog.csdn.net/huang_550/article/details/76492600
 public class DatabaseConfiguration {
 
-    // 数据源，使用 HikariCP
+    // 数据库连接池 Druid
 
 }
--- system/system-service-impl/src/main/java/cn/iocoder/mall/admin/config/ServiceExceptionConfiguration.java
+++ system/system-service-impl/src/main/java/cn/iocoder/mall/admin/config/ServiceExceptionConfiguration.java
@@ -11,13 +11,6 @@ public class ServiceExceptionConfiguration {
 
     @EventListener(ApplicationReadyEvent.class) // 可参考 https://www.cnblogs.com/ssslinppp/p/7607509.html
     public void initMessages() {
-//        从 service_exception_message.properties 加载错误码的方案
-//        Properties properties;
-//        try {
-//            properties = PropertiesLoaderUtils.loadAllProperties("classpath:service_exception_message.properties");
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
         for (AdminErrorCodeEnum item : AdminErrorCodeEnum.values()) {
             ServiceExceptionUtil.put(item.getCode(), item.getMessage());
         }

2b02b5b9e50a2bf36b02474c5cd3527f0dae8cb2
sin <2943460818@qq.com>
2019-05-16 10:55:32 +0800

    - SystemApplication 跑错地方了

--- system/system-application/src/main/java/cn/iocoder/mall/admin/application/SystemApplication.java
+++ system/system-application/src/main/java/cn/iocoder/mall/admin/application/SystemApplication.java
@@ -1,4 +1,4 @@
-package cn.iocoder.mall.admin.application.vo.oauth2;
+package cn.iocoder.mall.admin.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

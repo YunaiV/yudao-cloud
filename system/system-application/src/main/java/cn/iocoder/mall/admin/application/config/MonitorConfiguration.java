f29d427fb1f5a359b7ef3b97d58a9f015055cdf9
YunaiV <zhijiantianya@gmail.com>
2019-05-20 16:12:57 +0800

    - 后端：引入下 Metrics 相关的包

--- system/system-application/src/main/java/cn/iocoder/mall/admin/application/config/MonitorConfiguration.java
+++
@@ -1,9 +0,0 @@
-package cn.iocoder.mall.admin.application.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-//@EnablePrometheusEndpoint
-//@EnableSpringBootMetricsCollector
-public class MonitorConfiguration {
-}

4a2870e8e50c1cf9ed6de5c65befd7a6cc8d23b5
YunaiV <zhijiantianya@gmail.com>
2020-04-12 20:10:16 +0800

    将 promotion 模块接入 SCA Dubbo 和 Nacos Discovery 组件，同时将 Seata 修改成 SCA Seata 组件

--- promotion/promotion-service-impl/src/main/java/cn/iocoder/mall/promotion/biz/config/DatabaseConfiguration.java
+++ promotion/promotion-service-impl/src/main/java/cn/iocoder/mall/promotion/biz/config/DatabaseConfiguration.java
@@ -1,19 +1,8 @@
 package cn.iocoder.mall.promotion.biz.config;
 
-import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
-import io.seata.rm.datasource.DataSourceProxy;
-import io.seata.spring.annotation.GlobalTransactionScanner;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.context.annotation.Primary;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
 
 @Configuration
 @MapperScan("cn.iocoder.mall.promotion.biz.dao") // 扫描对应的 Mapper 接口
@@ -22,62 +11,5 @@ import javax.sql.DataSource;
 public class DatabaseConfiguration {
 
     // 数据源，使用 HikariCP
-
-    @Value("${spring.application.name}")
-    private String applicationId;
-
-//    @Autowired
-//    private DataSourceProperties dataSourceProperties;
-//
-////    @Bean // TODO 芋艿，加了就一直报错，后面在找原因。
-//    @Primary
-//    public DruidDataSource druidDataSource(){
-//        DruidDataSource druidDataSource = new DruidDataSource();
-//        druidDataSource.setUrl(dataSourceProperties.getUrl());
-//        druidDataSource.setUsername(dataSourceProperties.getUsername());
-//        druidDataSource.setPassword(dataSourceProperties.getPassword());
-//        druidDataSource.setDriverClassName(dataSourceProperties.getDriverClassName());
-//        druidDataSource.setInitialSize(0);
-//        druidDataSource.setMaxActive(180);
-//        druidDataSource.setMaxWait(60000);
-//        druidDataSource.setMinIdle(0);
-//        druidDataSource.setValidationQuery("Select 1 from DUAL");
-//        druidDataSource.setTestOnBorrow(false);
-//        druidDataSource.setTestOnReturn(false);
-//        druidDataSource.setTestWhileIdle(true);
-//        druidDataSource.setTimeBetweenEvictionRunsMillis(60000);
-//        druidDataSource.setMinEvictableIdleTimeMillis(25200000);
-//        druidDataSource.setRemoveAbandoned(true);
-//        druidDataSource.setRemoveAbandonedTimeout(1800);
-//        druidDataSource.setLogAbandoned(true);
-//        return druidDataSource;
-//    }
-
-    @Bean("druidDataSource")
-    @ConfigurationProperties("spring.datasource.druid")
-    public DruidDataSource druidDataSource(){
-        return DruidDataSourceBuilder.create().build();
-    }
-
-    @ConfigurationProperties(prefix = "spring.datasource")
-    @Primary
-    @Bean("dataSource")
-//    @Bean
-    @DependsOn("druidDataSource") // 解决多数据源，循环依赖的问题。主要发生点在 DataSourceInitializerInvoker
-    public DataSource dataSource() {
-        DruidDataSource druidDataSource = druidDataSource();
-        return new DataSourceProxy(druidDataSource);
-    }
-
-    /**
-     * 注册一个StatViewServlet
-     *
-     * @return global transaction scanner
-     */
-    @Bean
-    public GlobalTransactionScanner globalTransactionScanner() {
-        return new GlobalTransactionScanner(applicationId, "my_test_tx_group");
-        // TODO 芋艿，txServiceGroup 后续要编辑下
-    }
 
 }

099d2b45e5963fefb3328cc6248d635c03405cb3
sin-ning <sinning@aliyun.com>
2020-04-14 14:16:49 +0800

    - 修改：去掉端口号

--- admin-web/config/server/config/proxy.prod.config.js
+++ admin-web/config/server/config/proxy.prod.config.js
@@ -7,27 +7,27 @@ module.exports = {
     pathRewrite: {},
   },
   '/product-api/': {
-    target: 'http://api.shop.iocoder.cn:18099',
+    target: 'http://api.shop.iocoder.cn',
     changeOrigin: true,
     pathRewrite: {},
   },
   '/order-api/': {
-    target: 'http://api.shop.iocoder.cn:18099',
+    target: 'http://api.shop.iocoder.cn',
     changeOrigin: true,
     pathRewrite: {},
   },
   '/promotion-api/': {
-    target: 'http://api.shop.iocoder.cn:18099',
+    target: 'http://api.shop.iocoder.cn',
     changeOrigin: true,
     pathRewrite: {},
   },
   '/pay-api/': {
-    target: 'http://api.shop.iocoder.cn:18099',
+    target: 'http://api.shop.iocoder.cn',
     changeOrigin: true,
     pathRewrite: {},
   },
   '/user-api/': {
-    target: 'http://api.shop.iocoder.cn:18099',
+    target: 'http://api.shop.iocoder.cn',
     changeOrigin: true,
     pathRewrite: {},
   },

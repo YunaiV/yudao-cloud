fcc5180ebf6b48e4e69e2ae7010306ce357140ce
YunaiV <zhijiantianya@gmail.com>
2022-05-06 01:31:24 +0800

    初始化网关项目

---
+++ yudao-gateway/src/main/java/cn/iocoder/yudao/gateway/YudaoGatewayApplication.java
@@ -0,0 +1,13 @@
+package cn.iocoder.yudao.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YudaoGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YudaoGatewayApplication.class, args);
+    }
+
+}

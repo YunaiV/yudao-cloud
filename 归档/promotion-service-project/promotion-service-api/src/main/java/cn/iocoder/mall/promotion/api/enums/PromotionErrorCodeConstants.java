479a05b5d775265dda8f160afcdec1bccb523b7a
YunaiV <zhijiantianya@gmail.com>
2022-11-05 01:19:25 +0800

    移除商品推荐的逻辑

--- 归档/promotion-service-project/promotion-service-api/src/main/java/cn/iocoder/mall/promotion/api/enums/PromotionErrorCodeConstants.java
+++ 归档/promotion-service-project/promotion-service-api/src/main/java/cn/iocoder/mall/promotion/api/enums/PromotionErrorCodeConstants.java
@@ -9,15 +9,6 @@ import cn.iocoder.common.framework.exception.ErrorCode;
  */
 public interface PromotionErrorCodeConstants {
 
-    // ========== Banner 模块 ==========
-    ErrorCode BANNER_NOT_EXISTS = new ErrorCode(1006000000, "账号不存在");
-
-    // ========== PRODUCT RECOMMEND 模块 ==========
-    ErrorCode PRODUCT_RECOMMEND_NOT_EXISTS = new ErrorCode(1006001000, "商品推荐不存在");
-    ErrorCode PRODUCT_RECOMMEND_PRODUCT_NOT_EXISTS = new ErrorCode(1006001001, "商品不存在");
-    ErrorCode PRODUCT_RECOMMEND_EXISTS = new ErrorCode(1006001002, "该商品推荐已经存在");
-
-
     // ========== COUPON TEMPLATE 模块 ==========
     ErrorCode COUPON_TEMPLATE_NOT_EXISTS = new ErrorCode(1006002000, "优惠劵模板（码）不存在");
     ErrorCode COUPON_TEMPLATE_NOT_CARD = new ErrorCode(1006002001, "不是优惠劵模板");
@@ -33,8 +24,5 @@ public interface PromotionErrorCodeConstants {
     ErrorCode COUPON_CARD_NOT_MATCH = new ErrorCode(1006003002, "优惠劵不匹配，无法使用");
     ErrorCode COUPON_CARD_STATUS_NOT_UNUSED = new ErrorCode(1006003003, "优惠劵不处于待使用状态");
     ErrorCode COUPON_CARD_STATUS_NOT_USED = new ErrorCode( 1006003004, "优惠劵不处于已使用状态");
-
-    // ========== PRICE 模块 ==========
-    ErrorCode PRICE_PRODUCT_SKU_NOT_EXISTS = new ErrorCode(1006004000, "有不存在的商品！");
 
 }

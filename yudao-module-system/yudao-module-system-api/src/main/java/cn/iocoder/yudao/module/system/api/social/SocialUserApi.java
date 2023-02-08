e24d9dec01104fe67ac45e840a6c67c4191722f5
YunaiV <zhijiantianya@gmail.com>
2023-02-08 21:59:00 +0800

    修复 SocialUserApi 未提供 RPC 支持，导致带 token 访问时提示需登录

--- yudao-module-system/yudao-module-system-api/src/main/java/cn/iocoder/yudao/module/system/api/social/SocialUserApi.java
+++ yudao-module-system/yudao-module-system-api/src/main/java/cn/iocoder/yudao/module/system/api/social/SocialUserApi.java
@@ -19,7 +19,7 @@ public interface SocialUserApi {
 
     String PREFIX = ApiConstants.PREFIX + "/social-user";
 
-    @GetMapping("/get-authorize-url")
+    @GetMapping(PREFIX + "/get-authorize-url")
     @ApiOperation("获得社交平台的授权 URL")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "type", value = "社交平台的类型", example = "1", required = true, dataTypeClass = Integer.class),
@@ -28,15 +28,15 @@ public interface SocialUserApi {
     CommonResult<String> getAuthorizeUrl(@RequestParam("type") Integer type,
                                          @RequestParam("redirectUri") String redirectUri);
 
-    @PostMapping("/bind")
+    @PostMapping(PREFIX + "/bind")
     @ApiOperation("绑定社交用户")
     CommonResult<Boolean> bindSocialUser(@Valid @RequestBody SocialUserBindReqDTO reqDTO);
 
-    @DeleteMapping("/unbind")
+    @DeleteMapping(PREFIX + "/unbind")
     @ApiOperation("取消绑定社交用户")
     CommonResult<Boolean> unbindSocialUser(@Valid @RequestBody SocialUserUnbindReqDTO reqDTO);
 
-    @GetMapping("/get-bind-user-id")
+    @GetMapping(PREFIX + "/get-bind-user-id")
     @ApiOperation("获得社交用户的绑定用户编号")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "userType", value = "用户类型", example = "2", required = true, dataTypeClass = Integer.class),

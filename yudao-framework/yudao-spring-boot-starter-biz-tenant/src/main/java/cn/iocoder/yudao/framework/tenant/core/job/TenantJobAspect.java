9ec0ce48e2ac385e445bf7972071bf6ea760f12d
YunaiV <zhijiantianya@gmail.com>
2023-09-16 14:06:31 +0800

    优化 tenant 组件的注释

--- yudao-framework/yudao-spring-boot-starter-biz-tenant/src/main/java/cn/iocoder/yudao/framework/tenant/core/job/TenantJobAspect.java
+++ yudao-framework/yudao-spring-boot-starter-biz-tenant/src/main/java/cn/iocoder/yudao/framework/tenant/core/job/TenantJobAspect.java
@@ -21,6 +21,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * 多租户 JobHandler AOP
+ * 任务执行时，会按照租户逐个执行 Job 的逻辑
+ *
+ * 注意，需要保证 JobHandler 的幂等性。因为 Job 因为某个租户执行失败重试时，之前执行成功的租户也会再次执行。
+ *
+ * @author 芋道源码
+ */
 @Aspect
 @RequiredArgsConstructor
 @Slf4j
@@ -50,7 +58,8 @@ public class TenantJobAspect {
 
         // 逐个租户，执行 Job
         Map<Long, String> results = new ConcurrentHashMap<>();
-        tenantIds.parallelStream().forEach(tenantId -> { // TODO 芋艿：先通过 parallel 实现并行；1）多个租户，是一条执行日志；2）异常的情况
+        tenantIds.parallelStream().forEach(tenantId -> {
+            // TODO 芋艿：先通过 parallel 实现并行；1）多个租户，是一条执行日志；2）异常的情况
             TenantUtils.execute(tenantId, () -> {
                 try {
                     joinPoint.proceed();

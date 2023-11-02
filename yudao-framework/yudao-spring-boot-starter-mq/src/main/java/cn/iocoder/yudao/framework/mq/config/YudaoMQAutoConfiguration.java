2450d7afdc9dd3efdef9307e02134f4cb253cfb8
YunaiV <zhijiantianya@gmail.com>
2023-11-02 13:06:05 +0800

    mq：移除默认的 spring cloud stream 和 bus，使用原生的 spring-rocketmq、spring-kafka、spring-rabbitmq 替代，降低学习成本，提升使用灵活性。

--- yudao-framework/yudao-spring-boot-starter-mq/src/main/java/cn/iocoder/yudao/framework/mq/config/YudaoMQAutoConfiguration.java
+++ yudao-framework/yudao-spring-boot-starter-mq/src/main/java/cn/iocoder/yudao/framework/mq/config/YudaoMQAutoConfiguration.java
@@ -1,15 +1,6 @@
 package cn.iocoder.yudao.framework.mq.config;
 
-import com.alibaba.cloud.stream.binder.rocketmq.convert.RocketMQMessageConverter;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.converter.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 消息队列配置类
@@ -18,20 +9,5 @@ import java.util.List;
  */
 @AutoConfiguration
 public class YudaoMQAutoConfiguration {
-
-    /**
-     * 覆盖 {@link RocketMQMessageConverter} 的配置，去掉 fastjson 的转换器，解决不兼容的问题
-     */
-    @Bean(RocketMQMessageConverter.DEFAULT_NAME)
-    @ConditionalOnMissingBean(name = { RocketMQMessageConverter.DEFAULT_NAME })
-    public CompositeMessageConverter rocketMQMessageConverter() {
-        List<MessageConverter> messageConverters = new ArrayList<>();
-        ByteArrayMessageConverter byteArrayMessageConverter = new ByteArrayMessageConverter();
-        byteArrayMessageConverter.setContentTypeResolver(null);
-        messageConverters.add(byteArrayMessageConverter);
-        messageConverters.add(new StringMessageConverter());
-        messageConverters.add(new MappingJackson2MessageConverter());
-        return new CompositeMessageConverter(messageConverters);
-    }
 
 }

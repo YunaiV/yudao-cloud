02693836b29ea62d9272acbffad2f555407eb741
YunaiV <zhijiantianya@gmail.com>
2023-11-02 12:57:25 +0800

    mq：默认使用 event 替代 spring cloud stream

--- yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/mq/consumer/sms/SmsSendConsumer.java
+++ yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/mq/consumer/sms/SmsSendConsumer.java
@@ -3,10 +3,11 @@ package cn.iocoder.yudao.module.system.mq.consumer.sms;
 import cn.iocoder.yudao.module.system.mq.message.sms.SmsSendMessage;
 import cn.iocoder.yudao.module.system.service.sms.SmsSendService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.function.Consumer;
 
 /**
  * 针对 {@link SmsSendMessage} 的消费者
@@ -15,14 +16,16 @@ import java.util.function.Consumer;
  */
 @Component
 @Slf4j
-public class SmsSendConsumer implements Consumer<SmsSendMessage> {
+public class SmsSendConsumer {
 
     @Resource
     private SmsSendService smsSendService;
 
-    @Override
-    public void accept(SmsSendMessage message) {
-        log.info("[accept][消息内容({})]", message);
+    @EventListener
+    @Async // Spring Event 默认在 Producer 发送的线程，通过 @Async 实现异步
+    public void onMessage(SmsSendMessage message) {
+        log.info("[onMessage][消息内容({})]", message);
         smsSendService.doSendSms(message);
     }
+
 }

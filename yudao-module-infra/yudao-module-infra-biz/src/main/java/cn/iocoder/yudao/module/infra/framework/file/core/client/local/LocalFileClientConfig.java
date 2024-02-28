161b5e5bfc1d5c343718f228457945fa21cc9f60
YunaiV <zhijiantianya@gmail.com>
2024-02-28 18:59:49 +0800

    ✨ 全局：简化 file 组件，融合到 infra 模块

---
+++ yudao-module-infra/yudao-module-infra-biz/src/main/java/cn/iocoder/yudao/module/infra/framework/file/core/client/local/LocalFileClientConfig.java
@@ -0,0 +1,29 @@
+package cn.iocoder.yudao.module.infra.framework.file.core.client.local;
+
+import cn.iocoder.yudao.module.infra.framework.file.core.client.FileClientConfig;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+import org.hibernate.validator.constraints.URL;
+
+/**
+ * 本地文件客户端的配置类
+ *
+ * @author 芋道源码
+ */
+@Data
+public class LocalFileClientConfig implements FileClientConfig {
+
+    /**
+     * 基础路径
+     */
+    @NotEmpty(message = "基础路径不能为空")
+    private String basePath;
+
+    /**
+     * 自定义域名
+     */
+    @NotEmpty(message = "domain 不能为空")
+    @URL(message = "domain 必须是 URL 格式")
+    private String domain;
+
+}

5e9113a5c83f1fa3fa3ebe1ba881aa4d5d29e2e7
YunaiV <zhijiantianya@gmail.com>
2024-07-20 14:00:08 +0800

    【功能优化】全局：支持数据权限在 RPC 调用时，传递忽略数据权限的能力

--- yudao-framework/yudao-spring-boot-starter-biz-data-permission/src/main/java/cn/iocoder/yudao/framework/datapermission/core/rpc/DataPermissionRpcWebFilter.java
+++ yudao-framework/yudao-spring-boot-starter-biz-data-permission/src/main/java/cn/iocoder/yudao/framework/datapermission/core/rpc/DataPermissionRpcWebFilter.java
@@ -2,12 +2,12 @@ package cn.iocoder.yudao.framework.datapermission.core.rpc;
 
 import cn.iocoder.yudao.framework.datapermission.core.aop.DataPermissionContextHolder;
 import cn.iocoder.yudao.framework.datapermission.core.util.DataPermissionUtils;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Objects;
 

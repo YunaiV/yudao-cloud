b728e66282e0585c1e39d67bff516d99ec14b26e
YunaiV <zhijiantianya@gmail.com>
2024-07-20 17:57:31 +0800

    【功能新增】INFRA：增加 ConfigApi，支持参数配置的读取

--- yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/framework/rpc/config/RpcConfiguration.java
+++ yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/framework/rpc/config/RpcConfiguration.java
@@ -1,11 +1,12 @@
 package cn.iocoder.yudao.module.system.framework.rpc.config;
 
+import cn.iocoder.yudao.module.infra.api.config.ConfigApi;
 import cn.iocoder.yudao.module.infra.api.file.FileApi;
 import cn.iocoder.yudao.module.infra.api.websocket.WebSocketSenderApi;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
-@EnableFeignClients(clients = {FileApi.class, WebSocketSenderApi.class})
+@EnableFeignClients(clients = {FileApi.class, WebSocketSenderApi.class, ConfigApi.class})
 public class RpcConfiguration {
 }

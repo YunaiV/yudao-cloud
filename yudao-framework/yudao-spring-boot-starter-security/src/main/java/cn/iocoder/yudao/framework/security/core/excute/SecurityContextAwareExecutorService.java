8b6565c50decf8aa7580ba001fbed12cb9ac9577
srd19933311042 <71141650@chinatelecom.cn>
2024-12-24 10:54:42 +0800

    1、feat：之前遇到过一个现象，在普通用户登录过之后在重新登录最大权限的用户，整个线程里存储的用户信息还是上一个用户的现象, 很多模块提示无权限。原因：在CacheLoader中的load方法做缓存处理时，通过SecurityFrameworkUtils.getLoginUser()还是上一位（本地打断点试过），后续咱们主分支直接把LoadingCache改为同步的了，不清楚是否有性能影响，推荐下阿里巴巴的 TtlExecutors 执行器，此处手动为当前load程序内执行的线程设置 context 。

---
+++ yudao-framework/yudao-spring-boot-starter-security/src/main/java/cn/iocoder/yudao/framework/security/core/excute/SecurityContextAwareExecutorService.java
@@ -0,0 +1,103 @@
+package cn.iocoder.yudao.framework.security.core.excute;
+
+import cn.iocoder.yudao.framework.security.core.LoginUser;
+import cn.iocoder.yudao.framework.security.core.util.SecurityFrameworkUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
+
+/**
+ * SecurityContextAwareExecutorService
+ *
+ * @author dxsc_jhj
+ * @date 2024-10-17 01:21
+ **/
+@Slf4j
+public class SecurityContextAwareExecutorService implements ExecutorService {
+
+    private final ExecutorService delegate;
+
+    public SecurityContextAwareExecutorService(ExecutorService delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        LoginUser loginUser = SecurityFrameworkUtils.getLoginUser();
+        delegate.execute(() -> {
+            try {
+                SecurityContextHolder.setContext(securityContext);
+                command.run();
+            } finally {
+                log.trace("清理Context");
+                SecurityContextHolder.clearContext();
+            }
+        });
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public List<Runnable> shutdownNow() {
+        return null;
+    }
+
+    @Override
+    public boolean isShutdown() {
+        return false;
+    }
+
+    @Override
+    public boolean isTerminated() {
+        return false;
+    }
+
+    @Override
+    public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public <T> Future<T> submit(Callable<T> task) {
+        return null;
+    }
+
+    @Override
+    public <T> Future<T> submit(Runnable task, T result) {
+        return null;
+    }
+
+    @Override
+    public Future<?> submit(Runnable task) {
+        return null;
+    }
+
+    @Override
+    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
+        return null;
+    }
+
+    @Override
+    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
+        return null;
+    }
+
+    @Override
+    public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
+        return null;
+    }
+
+    @Override
+    public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+        return null;
+    }
+}
+// 后续实现方法自行补全

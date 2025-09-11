57978353f574360af8251565c72912cb58d62ef1
hanchunyu <hanchunyu@privacycn.com>
2025-09-11 19:20:48 +0800

    fix: 修复Gateway网关使用Webflux,导致skywalking日志打印traceId失效。使用Webflux的插件，获取TraceId后手动设置到MDC.

--- yudao-framework/yudao-spring-boot-starter-monitor/src/main/java/cn/iocoder/yudao/framework/tracer/core/util/TracerFrameworkUtils.java
+++ yudao-framework/yudao-spring-boot-starter-monitor/src/main/java/cn/iocoder/yudao/framework/tracer/core/util/TracerFrameworkUtils.java
@@ -2,6 +2,9 @@ package cn.iocoder.yudao.framework.tracer.core.util;
 
 import io.opentracing.Span;
 import io.opentracing.tag.Tags;
+import org.apache.skywalking.apm.toolkit.webflux.WebFluxSkyWalkingTraceContext;
+import org.slf4j.MDC;
+import org.springframework.web.server.ServerWebExchange;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -14,6 +17,10 @@ import java.util.Map;
  * @author 芋道源码
  */
 public class TracerFrameworkUtils {
+    /**
+     * 链路追踪的 TraceId 不存在时，使用的默认值
+     */
+    private static final String TRACE_ID_DEFAULT = "N/A";
 
     /**
      * 将异常记录到 Span 中，参考自 com.aliyuncs.utils.TraceUtils
@@ -43,4 +50,74 @@ public class TracerFrameworkUtils {
         return errorLogs;
     }
 
+    /**
+     * 获取 TraceId
+     *
+     * @param exchange ServerWebExchange
+     * @return TraceId
+     *
+     */
+    public static String getTraceId(ServerWebExchange exchange) {
+        try {
+            String traceId = WebFluxSkyWalkingTraceContext.traceId(exchange);
+            return TRACE_ID_DEFAULT.equals(traceId) ? null : traceId;
+        } catch (Throwable ignored) {
+            return null;
+        }
+    }
+
+    /**
+     * 将 TraceId 放入 MDC，默认 key=tid
+     *
+     * @param exchange ServerWebExchange
+     *
+     */
+    public static void putTidIntoMdc(ServerWebExchange exchange) {
+        putTidIntoMdc(exchange, "tid");
+    }
+
+    /**
+     * 将 TraceId 放入 MDC，指定 key
+     *
+     * @param exchange ServerWebExchange
+     * @param key      MDC key
+     *
+     */
+    public static void putTidIntoMdc(ServerWebExchange exchange, String key) {
+        String traceId = getTraceId(exchange);
+        if (traceId != null) {
+            MDC.put(key, traceId);
+        }
+    }
+
+    /**
+     * 将 TraceId 放入 MDC，传入 traceId 和 key
+     *
+     * @param traceId TraceId
+     * @param key     MDC key
+     *
+     */
+    public static void putTidIntoMdc(String traceId, String key) {
+        if (traceId != null && !TRACE_ID_DEFAULT.equals(traceId)) {
+            MDC.put(key, traceId);
+        }
+    }
+
+    /**
+     * 从 MDC 中移除 TraceId，默认 key=tid
+     *
+     */
+    public static void removeTidFromMdc() {
+        removeTidFromMdc("tid");
+    }
+
+    /**
+     * 从 MDC 中移除 TraceId，指定 key
+     *
+     * @param key MDC key
+     *
+     */
+    public static void removeTidFromMdc(String key) {
+        MDC.remove(key);
+    }
 }

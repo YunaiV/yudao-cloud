bf0c7dadc799ea5659718b3228966e0debc1eb80
YunaiV <zhijiantianya@gmail.com>
2025-10-02 18:51:27 +0800

    【同步】BOOT 和 CLOUD 的功能

--- yudao-framework/yudao-spring-boot-starter-web/src/main/java/cn/iocoder/yudao/framework/web/config/YudaoWebAutoConfiguration.java
+++ yudao-framework/yudao-spring-boot-starter-web/src/main/java/cn/iocoder/yudao/framework/web/config/YudaoWebAutoConfiguration.java
@@ -48,7 +48,7 @@ public class YudaoWebAutoConfiguration {
 
             @Override
             public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
-                var mapping = new RequestMappingHandlerMapping();
+                RequestMappingHandlerMapping mapping = new RequestMappingHandlerMapping();
                 // 实例化时就带上前缀
                 mapping.setPathPrefixes(buildPathPrefixes(webProperties));
                 return mapping;

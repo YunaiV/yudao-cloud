ec8577bdd961fe76e848aff8df2217c05b7e73fd
YunaiV <zhijiantianya@gmail.com>
2025-12-13 12:32:46 +0800

    【同步】BOOT 和 CLOUD 的功能

--- yudao-module-infra/yudao-module-infra-server/src/main/java/cn/iocoder/yudao/module/infra/controller/admin/logger/ApiErrorLogController.java
+++ yudao-module-infra/yudao-module-infra-server/src/main/java/cn/iocoder/yudao/module/infra/controller/admin/logger/ApiErrorLogController.java
@@ -50,6 +50,15 @@ public class ApiErrorLogController {
         return success(true);
     }
 
+    @GetMapping("/get")
+    @Operation(summary = "获得 API 错误日志")
+    @Parameter(name = "id", description = "编号", required = true, example = "1024")
+    @PreAuthorize("@ss.hasPermission('infra:api-error-log:query')")
+    public CommonResult<ApiErrorLogRespVO> getApiErrorLog(@RequestParam("id") Long id) {
+        ApiErrorLogDO apiErrorLog = apiErrorLogService.getApiErrorLog(id);
+        return success(BeanUtils.toBean(apiErrorLog, ApiErrorLogRespVO.class));
+    }
+
     @GetMapping("/page")
     @Operation(summary = "获得 API 错误日志分页")
     @PreAuthorize("@ss.hasPermission('infra:api-error-log:query')")
@@ -63,7 +72,7 @@ public class ApiErrorLogController {
     @PreAuthorize("@ss.hasPermission('infra:api-error-log:export')")
     @ApiAccessLog(operateType = EXPORT)
     public void exportApiErrorLogExcel(@Valid ApiErrorLogPageReqVO exportReqVO,
-              HttpServletResponse response) throws IOException {
+                                       HttpServletResponse response) throws IOException {
         exportReqVO.setPageSize(PageParam.PAGE_SIZE_NONE);
         List<ApiErrorLogDO> list = apiErrorLogService.getApiErrorLogPage(exportReqVO).getList();
         // 导出 Excel

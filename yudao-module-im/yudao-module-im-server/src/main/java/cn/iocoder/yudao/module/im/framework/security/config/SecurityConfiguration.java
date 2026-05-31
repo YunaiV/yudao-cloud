2ba231aa551ca085efbc2e68f3caeaa6b3a28ccf
YunaiV <zhijiantianya@gmail.com>
2026-06-01 00:22:51 +0800

    【同步】BOOT 和 CLOUD 的功能（IM）解决启动报错问题

---
+++ yudao-module-im/yudao-module-im-server/src/main/java/cn/iocoder/yudao/module/im/framework/security/config/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package cn.iocoder.yudao.module.im.framework.security.config;
+
+import cn.iocoder.yudao.framework.security.config.AuthorizeRequestsCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+
+/**
+ * IM 模块的 Security 配置
+ */
+@Configuration(proxyBeanMethods = false, value = "imSecurityConfiguration")
+public class SecurityConfiguration {
+
+    @Bean("imAuthorizeRequestsCustomizer")
+    public AuthorizeRequestsCustomizer authorizeRequestsCustomizer() {
+        return new AuthorizeRequestsCustomizer() {
+
+            @Override
+            public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+                // Swagger 接口文档
+                registry.requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/webjars/**").permitAll()
+                        .requestMatchers("/swagger-ui").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll();
+                // Spring Boot Actuator 的安全配置
+                registry.requestMatchers("/actuator").permitAll()
+                        .requestMatchers("/actuator/**").permitAll();
+                // Druid 监控
+                registry.requestMatchers("/druid/**").permitAll();
+            }
+
+        };
+    }
+
+}

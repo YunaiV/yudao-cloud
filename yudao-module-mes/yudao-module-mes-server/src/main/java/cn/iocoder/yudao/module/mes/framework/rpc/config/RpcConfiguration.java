1653a7c4d4b7c75c2f911518316c03d69bc155b2
YunaiV <zhijiantianya@gmail.com>
2026-06-27 10:59:42 -0700

    fix(mes)：启动缺少 DictDataApi RPC 服务

--- yudao-module-mes/yudao-module-mes-server/src/main/java/cn/iocoder/yudao/module/mes/framework/rpc/config/RpcConfiguration.java
+++ yudao-module-mes/yudao-module-mes-server/src/main/java/cn/iocoder/yudao/module/mes/framework/rpc/config/RpcConfiguration.java
@@ -1,12 +1,13 @@
 package cn.iocoder.yudao.module.mes.framework.rpc.config;
 
 import cn.iocoder.yudao.module.system.api.dept.PostApi;
+import cn.iocoder.yudao.module.system.api.dict.DictDataApi;
 import cn.iocoder.yudao.module.system.api.permission.RoleApi;
 import cn.iocoder.yudao.module.system.api.user.AdminUserApi;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(value = "mesRpcConfiguration", proxyBeanMethods = false)
-@EnableFeignClients(clients = {AdminUserApi.class, PostApi.class, RoleApi.class})
+@EnableFeignClients(clients = {AdminUserApi.class, PostApi.class, RoleApi.class, DictDataApi.class})
 public class RpcConfiguration {
 }
